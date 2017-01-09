b1449915dca5f3e1bfbca93b0ab4b65c142eeef5
Geoff Simons <geoff.simons@gmail.com>
2017-01-09 14:36:11 -0800

    Empty routers

---
+++ route/biz-router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Router = require('express').Router;
+
+const bizRouter = module.exports = Router();
+
+bizRouter.post('/api/biz', function(req, res, next) {
+  //TODO: Implement POST /api/biz
+  next();
+});
