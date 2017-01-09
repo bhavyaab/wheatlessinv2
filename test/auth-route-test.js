f0c8c04b212dd4a934dad10fa0587c7fcb1517c8
Geoff Simons <geoff.simons@gmail.com>
2017-01-09 15:30:18 -0800

    Fixed auth route and user

--- test/auth-route-test.js
+++ test/auth-route-test.js
@@ -1,106 +1,143 @@
 'use strict';
 
-require('./lib/test-env.js');
-
 const expect = require('chai').expect;
+const request = require('superagent');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
-const request = require('superagent');
-const User = require('../model/user.js');
-const debug = require('debug')('wheatlessinv2:auth-route-test');
-
 mongoose.Promise = Promise;
-require('../server.js');
-const url = `http://localhost:${process.env.PORT}`;
 
-const testUser = {
-  username: 'test-User',
-  password: '1234',
-  email: 'test-user@test.com'
+const User = require('../model/user.js');
+
+//TODO: Refactor to use our /lib helpers
+require('../server.js');
+
+const exampleUser = {
+  username: 'theexampleplayer',
+  email: 'somebody@example.com',
+  password: '123abc'
 };
 
-describe('Auth routes', function(){
-  User.remove({});
-  describe('POST: /api/signup', function(){
-    after( done => {
-      User.remove({})
-      .then( () => done())
-      .catch(done);
-    });
-    debug('POST: /api/signup');
+const url = `http://localhost:${process.env.PORT}`;
 
-    it('should say invalid path', done => {
-      request.post(`${url}/api/sig`)
-      .end((err, res) => {
+function cleanup(done) {
+  User.remove({})
+  .then( () => done())
+  .catch(done);
+}
+
+describe('Auth Routes', function() {
+  describe('GET /some/bogus/route', function() {
+    it('should return a 404', done => {
+      request.get(`${url}/some/bogus/route`)
+      .end( (err, res) => {
         expect(res.status).to.equal(404);
         done();
       });
     });
+  }); // Bogus Routes
 
-    it('should say body is invalid', done => {
-      request.post(`${url}/api/signup`)
-      .end((err, res) => {
-        expect(res.status).to.equal(400);
-        done();
+  describe('POST /api/signup', function() {
+    describe('with a valid body', function() {
+      after( done => cleanup(done));
+
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end( (err, res) => {
+          if(err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
       });
-    });
-    it('should return a token', done => {
-      request.post(`${url}/api/signup`)
-      .send(testUser)
-      .end((err, res) => {
-        if(err) return done(err);
-        expect(res.status).to.equal(200);
-        expect(res).to.have.property('text');
-        done();
+    }); // valid body
+
+    describe('with a missing username', function() {
+      after( done => cleanup(done)); //Just in case?
+
+      it('should return a 400', done => {
+        request.post(`${url}/api/signup`)
+        .send({ email: exampleUser.email, password: exampleUser.password })
+        .end( (err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
       });
-    });
-  });
-  describe('GET: /api/signin', function(){
-    debug('GET: /api/signin');
+    }); // missing username
+
+    describe('with a missing password', function() {
+      after( done => cleanup(done)); //Just in case?
+
+      it('should return a 400', done => {
+        request.post(`${url}/api/signup`)
+        .send({ username: exampleUser.username, email: exampleUser.email })
+        .end( (err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    }); // missing password
+
+    describe('with a missing email', function() {
+      after( done => cleanup(done)); //Just in case?
+
+      it('should return a 400', done => {
+        request.post(`${url}/api/signup`)
+        .send({ username: exampleUser.username, password: exampleUser.password })
+        .end( (err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    }); // missing email
+
+  }); // POST /api/signup
+
+  describe('GET /api/signin', function() {
     before( done => {
-      var user = new User(testUser);
-      user.generatePasswordHash(user.password)
-      .then( () => user.save())
-      .then( () => user.generateToken())
-      .then( token => {
-        this.token = token;
-        done();
-      })
-      .catch(done);
-    });
-    after( done => {
-      User.remove({})
-      .then( () => done())
-      .catch(done);
-    });
-    it('should say invalid path', done => {
-      request.get(`${url}/api/sig`)
-      .set({Authorization: `Basic ${this.token}`})
-      .end((err, res) => {
-        expect(res.status).to.equal(404);
-        expect(res.text).to.equal('Cannot GET /api/sig\n');
+      let user = new User(exampleUser);
+      user.generatePasswordHash(exampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
         done();
       });
     });
 
-    it('should say UnauthorizedError', done => {
-      request.get(`${url}/api/signin`)
-      .end((err, res) => {
-        expect(res.status).to.equal(401);
-        expect(res.text).to.equal('UnauthorizedError');
+    after( done => cleanup(done));
+
+    describe('with a valid username and password', () => {
+      it('should return a token', done => {
+        request.get(`${url}/api/signin`)
+        .auth(exampleUser.username, exampleUser.password)
+        .end( (err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
       });
-      done();
-    });
-    it('should return a token', done => {
-      request.get(`${url}/api/signin`)
-      .set({Authorization: `Basic ${this.token}`})
-      .send(testUser)
-      .end((err, res) => {
-        if(err) return done(err);
-        expect(res.status).to.equal(200);
-        expect(res).to.have.property('text');
+    }); // valid username and password
+
+    describe('with incorrect password', () => {
+      it('should return a 401', done => {
+        request.get(`${url}/api/signin`)
+        .auth(exampleUser.username, 'not_the_real_password')
+        .end( (err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
       });
-      done();
-    });
-  });
+    }); // incorrect password
+
+    describe('unknown username', () => {
+      it('should return a 401', done => {
+        request.get(`${url}/api/signin`)
+        .auth('not_a_user', exampleUser.password)
+        .end( (err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    }); //unknown username
+  }); // GET /api/signin
 });
