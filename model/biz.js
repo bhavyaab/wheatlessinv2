dd2afba3decdb47c063b071eff5f55e0c9a72e26
Geoff Simons <geoff.simons@gmail.com>
2017-01-09 23:47:32 -0800

    Adding geo-search

--- model/biz.js
+++ model/biz.js
@@ -9,6 +9,18 @@ const bizSchema = Schema({
   name: { type: String, required: true },
   EIN: { type: String, required: true }, //TODO: How to validate format XX-XXXXXXX
   // geo: { Type: LAT_LONG } //TODO: Lookup geo Schema
+  loc: {
+    lng: {
+      type: Number,
+      min: -180,
+      max: 180
+    },
+    lat: {
+      type: Number,
+      min: -90,
+      max: 90
+    }
+  },
   address: { Type: String }, //TODO: Breakdown address into address object
   //TODO: Consider allowing more than one menu per biz.
   menu: { Type: Schema.Types.ObjectId },
@@ -16,5 +28,7 @@ const bizSchema = Schema({
   email: { Type: String },
   phone: { Type: String }
 });
+
+bizSchema.index({ 'loc': '2d' });
 
 module.exports = mongoose.model('biz', bizSchema);
