fc709afd7bb9481632dc5a8789ce9697225e9f98
JINHO PAIK <JMPAIK83@GMAIL.COM>
2017-01-09 21:21:19 -0800

    returned pic model and changed menu schema

--- model/menu.js
+++ model/menu.js
@@ -5,10 +5,8 @@ const Schema = mongoose.Schema;
 
   const menuSchema = Schema({
     //TODO: menuSchema needs to have an array of pics for multiple page menus
-  menuId: { type: Schema.Types.ObjectId, required: true, unique: true },
+  picId: { type: String, required: true, unique: true },
   businessId: { type: Mongoose.Schema.Types.ObjectId, required: true },
-  imageURI: { type: String, required: true, unique: true },
-  objectKey: { type: String, required: true, unique: true },
   isCompletelyGlutenFree: { type: Boolean, required: true },
   created: { type: Date, default: Date.now }
 }));
