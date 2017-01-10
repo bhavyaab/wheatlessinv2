8fcec453328635096b51f57819feb7818d1c454b
Kyler Dotson <kyler.dotson@gmail.com>
2017-01-10 12:07:34 -0800

    test doesn't pass yet

--- test/menu-route-test.js
+++ test/menu-route-test.js
@@ -14,14 +14,19 @@ const Menu = require('../model/menu.js');
 const Toggle = require('./lib/server-toggle.js');
 const mockUser = require('./lib/mock-user.js');
 require('./lib/test-env.js');
-const Server = require('../server.js');
+//const Server = require('../server.js');
+require('../server.js');
 
 const exampleMenu = {
   isCompletelyGlutenFree: true,
 };
 const exampleBusiness = {
+  EIN: '11-3456789',
+  loc: {
+    lat: 47.6182206,
+    lng: -122.3540207
+  },
   name: 'Kylers salmonella buffet',
-  EIN: '123-456-78',
   address: '123 fake st, seattle wa 98123',
   url: 'http://www.food.com',
   email: 'food@food.food',
@@ -41,46 +46,43 @@ function cleanup(done) {
 describe('Menu routes', function() {
 
   before( done => {
-    Toggle.serverOn(Server, done);
+    //Toggle.serverOn(Server, done);
 
-    mockUser.then( newUser => {
+    mockUser().then( newUser => {
       this.tempUser = newUser;
+      return this.tempUser;
+    })
+    .then( tempUser => {
+      exampleBusiness.userId = tempUser._id;
+      new Biz(exampleBusiness).save()
+      .then( savedBiz => {
+        exampleMenu.bizId = savedBiz._id;
+        this.tempBiz = savedBiz;
+        done();
+      }).catch(done);
     }).catch(done);
 
-    new Biz(exampleBusiness).save()
-    .then( savedBiz => {
-      exampleMenu.bizId = savedBiz._id;
-      this.tempBiz = savedBiz;
-      debug('this.tempBiz:', this.tempBiz);
-    })
-    .catch(err => done(err));
-
-    done();
   }); //before everything
 
-  after( done => {
-    Toggle.serverOn(Server, done);
-  }); //after everything
+  after( done => cleanup(done));
 
-  describe('POST /api/menu', function() {
+  describe('POST /api/biz/:bizId/menu', () => {
 
-    before( done => {
-      //this.tempMenu = new Menu(exampleMenu).save();
-      done();
-    });
+    // before( done => {
+    //   this.tempMenu = new Menu(exampleMenu).save();
+    //   done();
+    // });
 
-    after( done => cleanup(done));
+    //after( done => cleanup(done));
 
     describe('with valid auth and valid menu', () => {
       it('should return a new menu', done => {
-        // request.post(`${url}/api/biz/${this.tempBiz._id.toString()}/menu`)
-        // .set({authorization: `Bearer ${this.tempUser.token}`})
-        // .send()
-        // .end( (err, res) => {
-        //   expect(res.status).to.equal(200);
-        //   done();
-        // });
-        done();
+        request.post(`${url}/api/biz/${this.tempBiz._id.toString()}/menu`)
+        .set({authorization: `Bearer ${this.tempUser.token}`})
+        .end( (err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
       });
     }); // valid auth and menu
 
