8319c2b983ce075cb3d6e3792b01103ffe5a79fa
Bhavya Bharti <bhavyab@outlook.com>
2017-01-10 12:15:56 -0800

    invalid token test

--- test/biz-route-test.js
+++ test/biz-route-test.js
@@ -29,77 +29,103 @@ const testbiz = {
 const url = `http://localhost:${process.env.PORT}/api/biz`;
 
 describe('Biz-router-test', function(){
- debug('bizRouter');
- before( done => {
-   var user = new User(testUser);
-   user.generatePasswordHash(testUser.password)
-   .then( () => user.save())
-   .then( () => user.generateToken())
-   .then( token => {
-     this.token = token;
-     this.userId = user._id;
-     done();
-   })
-   .catch(done);
- })
- after( done => {
-   User.remove({})
-   .then( () => done())
-   .catch(done);
- })
- after( done => {
-   Biz.remove({})
-   .then( () => done())
-   .catch(done);
- })
- describe('Biz:POST route', () => {
-   debug('Biz: POST route');
-   it('should test for invalid path', done => {
-    debug('valid body with invalid request');
-    request.post(`${url}/abcd`)
-    .send(testbiz)
-    .end( (err, res) => {
-      expect(res.status).to.equal(404);
-      console.log('******************res.status', res.status);
-      // expect(Error.msg).to.equal('Unauthorized');
+  debug('bizRouter');
+  before( done => {
+    var user = new User(testUser);
+    user.generatePasswordHash(testUser.password)
+    .then( () => user.save())
+    .then( () => user.generateToken())
+    .then( token => {
+      this.token = token;
+      this.userId = user._id;
       done();
+    })
+    .catch(done);
+  });
+  before( done => {
+    var newUser = new User({
+      username: 'FakeUser',
+      email: 'fakeUser@test.com',
+      password: 'fake'});
+    newUser.generatePasswordHash(testUser.password)
+    .then( () => newUser.save())
+    .then( () => newUser.generateToken())
+    .then( token => {
+      this.newToken = token;
+      User.findByIdAndRemove(newUser._id, function (err, user) {
+        user.remove();
+        done();
+      });
+    })
+    .catch(done);
+  });
+
+  after( done => {
+    User.remove({})
+    .then( () => done())
+    .catch(done);
+  });
+  after( done => {
+    Biz.remove({})
+    .then( () => done())
+    .catch(done);
+  });
+  describe('Biz:POST route', () => {
+    debug('Biz: POST route');
+    it('should test for invalid path', done => {
+      debug('valid body with invalid request');
+      request.post(`${url}/abcd`)
+      .send(testbiz)
+      .end( (err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
     });
-   });
-   it('should test for missing token', done => {
-    debug('valid body with invalid request');
-    request.post(`${url}`)
-    .send(testbiz)
-    .end( (err, res) => {
-      expect(res.status).to.equal(401);
-      console.log('******************res.status', res.status);
-      // expect(Error.msg).to.equal('Unauthorized');
-      done();
+    it('should test for missing token', done => {
+      debug('valid body with invalid request');
+      request.post(`${url}`)
+      .send(testbiz)
+      .end( (err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.text).to.equal('UnauthorizedError');
+        done();
+      });
     });
-   });
-   it('should test for invalid token', done => {
-    debug('valid body with invalid request');
-    request.post(`${url}`)
-    .set({Authorization: `Bearer ${this.token}notgoodtoken`})
-    .send(testbiz)
-    .end( (err, res) => {
-      expect(res.status).to.equal(401);
-      done();
+    it('should test for invalid token', done => {
+      debug('valid body with invalid request');
+      request.post(`${url}`)
+      .set({Authorization: `Bearer ${this.newToken}`})
+      .send(testbiz)
+      .end( (err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.text).to.equal('Not Found');
+        done();
+      });
     });
-   });
-   it('should POST biz', done => {
-    debug('valid body with valid request');
-    request.post(`${url}`)
-    .set({Authorization: `Bearer ${this.token}`})
-    .send(testbiz)
-    .end( (err, res) => {
-      if(err) return done(err);
-      expect(res.body.name).to.equal(testbiz.name);
-      expect(res.body.EIN).to.equal(testbiz.EIN);
-      expect(res.body).to.have.property('userId');
-      expect(res.body.userId).to.equal(`${this.userId}`);
-      expect(res.status).to.equal(200);
-      done();
+    it('missing body', done => {
+      debug('missing body with valid request');
+      request.post(`${url}`)
+      .set({Authorization: `Bearer ${this.token}`})
+      .end( (err, res) => {
+        expect(res.text).to.equal('BadRequestError');
+        expect(res.status).to.equal(400);
+        done();
+      });
     });
-   });
- });
+    it('should POST biz', done => {
+      debug('valid body with valid request');
+      request.post(`${url}`)
+      .set({Authorization: `Bearer ${this.token}`})
+      .send(testbiz)
+      .end( (err, res) => {
+        if(err) return done(err);
+        expect(res.body.name).to.equal(testbiz.name);
+        expect(res.body.EIN).to.equal(testbiz.EIN);
+        expect(res.body).to.have.property('userId');
+        expect(res.body.userId).to.equal(`${this.userId}`);
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+  });
 });
