65f73618e38138f947ec87aaa9ee7e89a0522a2f
Geoff Simons <geoff.simons@gmail.com>
2017-02-11 14:23:30 -0800

    Added deep loc checks

--- test/biz-route-test.js
+++ test/biz-route-test.js
@@ -50,21 +50,6 @@ describe('Biz-router-test', function(){
       done();
     });
   });
-
-
-  // before( done => {
-  //   var user = new User(testUser);
-  //   user.generatePasswordHash(testUser.password)
-  //   .then( () => user.save())
-  //   .then( () => user.generateToken())
-  //   .then( token => {
-  //     this.token = token;
-  //     this.userId = user._id;
-  //     done();
-  //   })
-  //   .catch(done);
-  // });
-
   before( done => {
     var newUser = new User({
       username: 'FakeUser',
@@ -82,15 +67,6 @@ describe('Biz-router-test', function(){
     })
     .catch(done);
   });
-  // before( done => {
-  //   examples.codefellows.userId = this.userId;
-  //   new Biz(examples.codefellows).save()
-  //   .then( biz => {
-  //     this.biz = biz;
-  //     done();
-  //   });
-  // });
-  //TODO: Promise.all() | helper function
   after( done => {
     User.remove({})
     .then( () => done())
@@ -159,37 +135,22 @@ describe('Biz-router-test', function(){
         .send(examples.codefellows)
         .end( (err, res) => {
           if(err) return done(err);
+          expect(res.status).to.equal(200);
           expect(res.body.name).to.equal(examples.codefellows.name);
           expect(res.body.EIN).to.equal(examples.codefellows.EIN);
           expect(res.body).to.have.property('userId');
           expect(res.body.userId).to.equal(`${this.userId}`);
-          expect(res.status).to.equal(200);
+          expect(res.body.address).to.equal(examples.codefellows.address);
+          let expectedLoc = geo(examples.codefellows.address).location;
+          expect(res.body.loc).to.deep.equal(expectedLoc);
           this.biz = res.body;
           debug('this.biz:',this.biz);
           done();
         });
       });
     });
-
-    // describe('valid post with address', () => {
-    //   it('should status 200', done => {
-    //     request.post(url)
-    //     .set({Authorization: `Bearer ${this.token}`})
-    //     .send(examples.codefellows)
-    //     .end( (err, res) => {
-    //       if(err) return done(err);
-    //       expect(res.status).to.equal(200);
-    //       expect(res.body.name).to.equal(examples.codefellows.name);
-    //       expect(res.body.EIN).to.equal(examples.codefellows.EIN);
-    //       expect(res.body).to.have.property('userId');
-    //       expect(res.body.userId).to.equal(`${this.userId}`);
-    //       // expect(res.body.loc.lat).to.equal(geomock.location.lat);
-    //       // expect(res.body.loc.lng).to.equal(geomock.location.lng);
-    //       done();
-    //     });
-    //   });
-    // });
   });
+
   describe('GET: api/biz/:id', () => {
     describe('invalid path', () => {
       it('expect error 404', done => {
@@ -257,27 +218,12 @@ describe('Biz-router-test', function(){
         });
       });
     });
-    // describe('valid id', () => {
-    //   it('expect res status 200', done => {
-    //     request.put(`${url}/${this.biz._id}`)
-    //     .set({authorization: `Bearer ${this.token}`})
-    //     //TODO: This should send a body, currently a false positive
-    //     .end( (err, res) => {
-    //       if(err) return done(err);
-    //       expect(res.status).to.equal(200);
-    //       expect(res.body.name).to.equal(examples.codefellows.name);
-    //       expect(res.body._id).to.equal(`${this.biz._id}`);
-    //       expect(res.body.EIN).to.equal(examples.codefellows.EIN);
-    //       done();
-    //     });
-    //   });
-    // });
-    describe('valid id, update address ***********************************', () => {
+    describe('valid id', () => {
       it('expect res status 200', done => {
-        // debug('updating to:', examples.crocodile);
         debug('this.biz:', this.biz);
         request.put(`${url}/${this.biz._id}`)
         .set({authorization: `Bearer ${this.token}`})
+        //TODO: send and verify updates for all fields?
         .send({
           name: examples.crocodile.name,
           address: examples.crocodile.address
@@ -287,7 +233,9 @@ describe('Biz-router-test', function(){
           expect(res.status).to.equal(200);
           expect(res.body.name).to.equal(examples.crocodile.name);
           expect(res.body._id).to.equal(`${this.biz._id}`);
-          //TODO: expect res.body.loc to deep eq geo.crocodile
+          expect(res.body.address).to.equal(examples.crocodile.address);
+          let expectedLoc = geo(examples.crocodile.address).location;
+          expect(res.body.loc).to.deep.equal(expectedLoc);
           done();
         });
       });
